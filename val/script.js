b683118f718902428b6b81487b4f1e59b9066fea
KenCartagenas <2402256@ub.edu.ph>
2026-02-14 17:56:38 +0800

    final

--- val/script.js
+++ val/script.js
@@ -1,11 +1,18 @@
 const keys = document.querySelectorAll('.btn');
-const tiles = document.querySelectorAll('.tile');
+const tiles = document.querySelectorAll('.front');
+const cards = document.querySelectorAll('.card');
+const nobtn = document.getElementById('no');
+const yesbtn = document.getElementById('yes');
 
+let bscolors = ['0 4px 0 #a81818', '0 4px 0 #a86e18', '0 4px 0 #a89e18', '0 4px 0 #18a843',
+              '0 4px 0 #1832a8', '0 4px 0 #8618a8', '0 4px 0 #a8189c'];
+let colors = ['#ff2323', '#ffa724', '#fff025', '#27ff68', '#244cff', '#cc25ff', '#ff24ed'];
 let x = 1;
 let y = 1;
 let letter = '';
 let word = '';
 let answer = 'LOVEU';
+let clicks = 1;
 
 keys.forEach(key => {
     key.addEventListener('click', () => {
@@ -29,7 +36,7 @@ function updatetile(n) {
         y += 1;
         if (y > 5) {
             keys.disabled = true;
-            document.getElementById('btns').style.display = 'flex';
+            document.getElementById('btn1').style.display = 'flex';
         }
         return;
     } else if (n == 'DEL') {
@@ -49,16 +56,16 @@ function updatetile(n) {
         return;
     
     const tileid = `r${y}t${x}`;
+    
     const tile = document.getElementById(tileid);
     word += n;
-    
     tile.textContent = n;
     x += 1;
 }
 
 function compare() {
     for (let i = 0; i < 5; i++) {
-        let tileid = `r${y}t${i + 1}`;
+        let tileid = `r${y}t${i + 1}t`;
         if (word[i] == answer[i]) {
             document.getElementById(tileid).style.background = 'green';
             document.getElementById(word[i]).style.background = 'green';
@@ -76,7 +83,7 @@ function compare() {
 }
 
 document.getElementById('play').addEventListener('click', () => {
-    document.getElementById('btns').style.display = 'none';
+    document.getElementById('btn1').style.display = 'none';
     x = 1;
     y = 1;
     word = '';
@@ -88,7 +95,7 @@ document.getElementById('play').addEventListener('click', () => {
     tiles.forEach(tile => {
         tile.style.boxShadow = '0 4px 0 #555555';
         tile.style.background = 'linear-gradient(to bottom, #4d4d4d 0%, #424242 100%)';
-        tile.textContent = '';
+        tile.textContent = ' ';
     });
 });
 
@@ -101,8 +108,62 @@ async function animate() {
     await sleep(4000);
     document.getElementById('keyboard').style.display = 'none';
     document.getElementById('ttl').style.display = 'none';
+    document.querySelectorAll('.back').forEach((back, index) => {
+        back.style.boxShadow = bscolors[Math.floor(index / 5)];
+        back.style.background = colors[Math.floor(index / 5)];
+    });
+    cards.forEach((card, index) => {
+        setTimeout(() => {
+            if(card.id == 'message')
+                return;
+            card.classList.toggle("flip");
+        }, index * 150);
+    });
+    document.querySelector('.bg-layer').classList.add("active");
+    await sleep(4000);
+    document.getElementById('btn2').style.display = 'flex';
 }
+
+nobtn.addEventListener('click', () => {
+    const top = Math.floor(Math.random() * (80 - 20 + 1) + 20);
+    const right = Math.floor(Math.random() * (80 - 20 + 1) + 20);
+    nobtn.style.position = 'absolute';
+    nobtn.style.top =  `${top}%`;
+    nobtn.style.right = `${right}%`;
+    nobtn.style.transform = `scale(${.75 - (clicks * .10)})`;
+    yesbtn.style.transform = `scale(${1.25 + (clicks * .10)})`;
+    clicks++;
+});
+
+yesbtn.addEventListener('click', async () => {
+    cards.forEach((card, index) => {
+        setTimeout(() => {
+            if (card.id == 'message')
+                return;
+            card.classList.add("animate");
+        }, index * 50); 
+    });
+    document.getElementById('btn2').style.display = 'none';
+    await sleep(4000);
+    document.getElementById('boxes').style.display = 'none';
+    document.getElementById('mcard').style.display = 'flex';
+    document.getElementById('btn3').style.display = 'flex';
+});
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+document.querySelectorAll('.message').forEach(msg => {
+    msg.addEventListener('click', () => {
+        msg.classList.toggle("flip");
+    });
+});
+
+document.getElementById('open').addEventListener('click', () => {
+    document.getElementById('popup-box').style.display = 'flex';
+});
+
+document.getElementById('popup-box').addEventListener('click', () => {
+    document.getElementById('popup-box').style.display = 'none';
+});
